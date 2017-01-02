798c45ee140f08892b76d342d2c832c3c48cd97e
Thomas Maloney <thomastnf@live.com>
2017-01-02 01:37:57 -0600

    Initial Commit

---
+++ Discord User Simulator/Discord User Simulator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_User_Simulator
+{
+    public class Program
+    {
+        static void Main(string[] args) => new Program().Run(args);
+
+        public void Run(string[] args)
+        {
+            
+        }
+    }
+}
